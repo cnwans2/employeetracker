d63cb50a137da31d1c6f35bf01198aa84b73daf4
Cnwans2 <crystal.nwansi26@gmail.com>
2020-04-09 20:46:30 -0500

    ugh

--- db/index.js
+++ db/index.js
@@ -88,7 +88,8 @@ class employee_db {
       departmentId
     );
 //This will get all employess who are managers.
-  findAllEmployeesByManager(managerId){
+  findAllEmployeesByManager(managerId) 
+  {
     return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
       managerId
---
+++ index.js
@@ -0,0 +1,82 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+
+  console.log(logoText);
+
+  loadMainPrompts();
+}
+
+async function loadMainPrompts() {
+  const { choice } = await prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View Employees",
+          value: "view employee"
+        },
+        {
+          name: "View Employees By Department",
+          value: "Departments"
+        },
+        {
+          name: "View Employees By Manager",
+          value: "management"
+        },
+        {
+          name: "Add Employee",
+          value: "add employee"
+        },
+        {
+          name: "Remove Employee",
+          value: "remove employee"
+        },
+        {
+          name: "Update Employee Role",
+          value: "update employee"
+        {
+          name: "Update Employee Manager",
+          value: "update managment"
+        },
+        {
+          name: "View All Roles",
+          value: "view roles"
+        },
+        {
+          name: "Add Role",
+          value: "add role"
+        },
+        {
+          name: "Remove Role",
+          value: "remove role"
+        },
+        {
+          name: "View All Departments",
+          value: "view departments"
+        },
+        {
+          name: "Add Department",
+          value: "add department"
+        },
+        {
+          name: "Remove Department",
+          value: "remove department"
+        },
+        {
+          name: "Quit",
+          value: "QUIT"
+        }
+      ]
+    }
+  ]);
+
+ 
